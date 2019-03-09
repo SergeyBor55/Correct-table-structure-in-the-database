283f4b27d4314ca92d17aa2df3d9773e4cdab616
SergeyBor55 <43942010+SergeyBor55@users.noreply.github.com>
2019-03-09 13:47:04 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-1) Напишите SQL-запрос, который бы возвращал самого популярного автора за год. Запрос должен основываться на модели данных, 
+Напишите SQL-запрос, который бы возвращал самого популярного автора за год. Запрос должен основываться на модели данных, 
 которая описана вами.
 
 SELECT authors.name AS popular_authors FROM authors INNER JOIN books ON authors.id = books.authors_id 
@@ -7,12 +7,12 @@ WHERE books_issued.date_of_issue LIKE ('2018%')
 GROUP BY books.authors_id ORDER BY COUNT(books.authors_id) DESC LIMIT 1
 
 
-3) Напишите sql запрос, который вернёт id студентов и количество взятых ими книг по определённым вами id студентов.
+Напишите sql запрос, который вернёт id студентов и количество взятых ими книг по определённым вами id студентов.
 
 SELECT student_id, COUNT(*) as count_books, date_of_issue FROM books_issued GROUP BY student_id HAVING student_id IN(2,4)
 
 
-2) Напишите sql запрос, который вернёт название книги, имя автора, издательство, год издания, количество взятых экземпляров в библиотеке,
+Напишите sql запрос, который вернёт название книги, имя автора, издательство, год издания, количество взятых экземпляров в библиотеке,
 где определённый вами автор и количество взятых книг этого автора попадает в диапазон от 1-3.
 
 SELECT books.title, authors.name AS authors, publishings.title AS publishings, books.publication_year, 
